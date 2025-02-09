baec146fcdbc1faca7ef31b64a2515eb303f6da2
Dong Wang <dongwang@wangdongdong.wang>
2025-02-09 01:35:57 +0100

    Update cors_bypass.js

--- cors_bypass.js
+++ cors_bypass.js
@@ -1,3 +1,21 @@
 let headers = $response.headers;
-headers["Access-Control-Allow-Origin"] = "https://visa.vfsglobal.com"; // 只保留一个值
+
+// 统一大小写，避免浏览器误解
+if (headers["access-control-allow-origin"]) {
+    delete headers["access-control-allow-origin"];
+}
+if (headers["Access-Control-Allow-Origin"]) {
+    delete headers["Access-Control-Allow-Origin"];
+}
+
+// 重新添加正确的 CORS 头
+headers["Access-Control-Allow-Origin"] = "https://visa.vfsglobal.com";
+headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
+headers["Access-Control-Allow-Headers"] = "*";
+
+// 确保 Vary 头不会干扰 CORS
+if (headers["Vary"]) {
+    delete headers["Vary"];
+}
+
 $done({headers});
